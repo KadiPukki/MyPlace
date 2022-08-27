0a7e2f00ba41e8464d5c0afbdd33e22bb84c41e5
Kadidja Pukki <kadidjatoure@hotmail.com>
2022-08-27 10:57:30 +0300

    menu

---
+++ assets/js/menu.js
@@ -0,0 +1,5 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navMenu = document.querySelector('.nav-menu');
+menuBtn.addEventListener('click', () =>{
+    navMenu.classList.toggle('mobile-menu');
+})
